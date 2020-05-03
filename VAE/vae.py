8002d1dafb4521ce4a4d464186e56ebec2442aa2
Yes <yes.92@kielnet.net>
2020-05-03 16:17:37 +0200

    bug durch varibale Batch_size

--- VAE/vae.py
+++ VAE/vae.py
@@ -30,7 +30,7 @@ class VAE(nn.Module):
         mu, logvar = self.enc(x)
         z = self.reparam(mu, logvar)
         output = self.dec(z)
-        return z, mu, logvar
+        return output, mu, logvar
 
 
 class Enoder(nn.Module):
@@ -38,20 +38,20 @@ class Enoder(nn.Module):
     def __init__(self, latent_dim):
         super().__init__()
 
-        self.conv1 = nn.Conv2d(1, 4, kernel_size=2, stride=2, padding=0, bias=True)
-        self.conv2 = nn.Conv2d(4, 8, kernel_size=2, stride=2, padding=0, bias=True)
-        self.conv3 = nn.Conv2d(8, 16, kernel_size=2, stride=2, padding=0, bias=True)
-        self.conv4 = nn.Conv2d(16, 16, kernel_size=2, stride=2, padding=0, bias=True)
-        self.linear_1 = nn.Linear(16, latent_dim)
-        self.linear_2 = nn.Linear(16, latent_dim)
+        self.conv1 = nn.Conv2d(1, 4, kernel_size=3, stride=1, padding=0, bias=True)
+        self.conv2 = nn.Conv2d(4, 8, kernel_size=3, stride=1, padding=0, bias=True)
+        self.conv3 = nn.Conv2d(8, 16, kernel_size=3, stride=1, padding=0, bias=True)
+        # self.conv4 = nn.Conv2d(1, 1, kernel_size=2, stride=2, padding=0, bias=True)
+        self.linear_1 = nn.Linear(22 * 22 * 16, latent_dim)
+        self.linear_2 = nn.Linear(22 * 22 * 16, latent_dim)
 
     def forward(self, x):
         x = F.tanh(self.conv1(x))
         x = F.tanh(self.conv2(x))
         x = F.tanh(self.conv3(x))
-        x = F.tanh(self.conv4(x))
-        z_mu = self.linear_1(x)
-        z_var = self.linear_2(x)
+        # x = F.tanh(self.conv4(x))
+        z_mu = self.linear_1(x.view(x.size(0), -1))
+        z_var = self.linear_2(x.view(x.size(0), -1))
 
         return z_mu, z_var
 
@@ -60,15 +60,15 @@ class Decoder(nn.Module):
 
     def __init__(self, latent_dim):
         super().__init__()
-        self.linear = nn.Linear(latent_dim, 16)
-        self.de_conv1 = nn.ConvTranspose2d(16, 8, kernel_size=2, stride=2, padding=0, bias=True)
-        self.de_conv2 = nn.ConvTranspose2d(8, 4, kernel_size=2, stride=2, padding=0, bias=True)
-        self.de_conv3 = nn.ConvTranspose2d(4, 2, kernel_size=2, stride=2, padding=0, bias=True)
+        self.linear = nn.Linear(latent_dim, 22 * 22 * 16)
+        self.de_conv1 = nn.ConvTranspose2d(16, 8, kernel_size=3, stride=1, padding=0, bias=True)
+        self.de_conv2 = nn.ConvTranspose2d(8, 4, kernel_size=3, stride=1, padding=0, bias=True)
+        self.de_conv3 = nn.ConvTranspose2d(4, 1, kernel_size=3, stride=1, padding=0, bias=True)
 
     def forward(self, x):
         x = F.tanh(self.linear(x))
-        x = F.tanh(self.de_conv1(x))
+        x = F.tanh(self.de_conv1(x.view(128, 16, 22, 22)))
         x = F.tanh(self.de_conv2(x))
-        x = self.de_conv3(x)
+        x = F.sigmoid(self.de_conv3(x))
 
         return x
